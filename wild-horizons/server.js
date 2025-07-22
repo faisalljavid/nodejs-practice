9161103d39970c73cce89c6c078dff5eb22932a5
faisalljavid <faisalljavid@gmail.com>
2025-07-22 15:30:14 +0530

    Add API functionality and data source for wild-horizons project
    - Updated package.json to set module type.
    - Implemented a basic HTTP server in server.js to serve destination data.
    - Created a new database module to fetch data from a predefined dataset.
    - Added a dataset of interesting places in data.js with relevant details.

--- wild-horizons/server.js
+++ wild-horizons/server.js
@@ -1,1 +1,32 @@
-console.log("We have a package.json!!")
+import { createServer } from 'node:http'
+import { getDataFromDB } from './database/db.js'
+
+const PORT = 8000
+
+const server = createServer(async (req, res) => {
+    const destinations = await getDataFromDB()
+
+    if (req.url === '/api' && req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200
+        res.end(JSON.stringify(destinations))
+    } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+        const continent = req.url.split('/').pop()
+        const filteredData = destinations.filter((destObj) => {
+            return destObj.continent.toLowerCase() === continent.toLowerCase()
+        })
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200
+        res.end(JSON.stringify(filteredData))
+    } else {
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 404
+        res.end(JSON.stringify({
+            error: "not found",
+            message: "The requested route does not exist"
+        })
+        )
+    }
+})
+
+server.listen(PORT, () => console.log(`Connected on PORT: ${PORT}`))
