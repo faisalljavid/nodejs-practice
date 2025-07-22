a2e518a40f3a448188db2012d148f5a630dbc5eb
faisalljavid <faisalljavid@gmail.com>
2025-07-22 16:19:49 +0530

    Modularize code

--- wild-horizons/server.js
+++ wild-horizons/server.js
@@ -1,5 +1,7 @@
 import { createServer } from 'node:http'
 import { getDataFromDB } from './database/db.js'
+import { sendJSONResponse } from './utils/sendJSONResponse.js'
+import { getDataByPathParams } from './utils/getDataByPathParams.js'
 
 const PORT = 8000
 
@@ -7,26 +9,25 @@ const server = createServer(async (req, res) => {
     const destinations = await getDataFromDB()
 
     if (req.url === '/api' && req.method === 'GET') {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 200
-        res.end(JSON.stringify(destinations))
+        sendJSONResponse(res, 200, destinations)
+
     } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
         const continent = req.url.split('/').pop()
-        const filteredData = destinations.filter((destObj) => {
-            return destObj.continent.toLowerCase() === continent.toLowerCase()
-        })
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 200
-        res.end(JSON.stringify(filteredData))
+        const filteredData = getDataByPathParams(destinations, 'continent', continent)
+        sendJSONResponse(res, 200, filteredData)
+
+    } else if (req.url.startsWith('/api/country') && req.method === 'GET') {
+        const country = req.url.split('/').pop()
+        const filteredData = getDataByPathParams(destinations, 'country', country)
+        sendJSONResponse(res, 200, filteredData)
+
     } else {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 404
-        res.end(JSON.stringify({
+        sendJSONResponse(res, 404, ({
             error: "not found",
             message: "The requested route does not exist"
-        })
-        )
+        }))
     }
+
 })
 
 server.listen(PORT, () => console.log(`Connected on PORT: ${PORT}`))
