2e068a4e782bee3a6fff0b528c4131826fa228cb
faisalljavid <faisalljavid@gmail.com>
2025-07-24 00:35:03 +0530

    Enhance sanitizeInput function to strip HTML tags from non-text fields

--- spooky-sightings/utils/sanitizeInput.js
+++ spooky-sightings/utils/sanitizeInput.js
@@ -1,10 +1,18 @@
 import sanitizeHtml from 'sanitize-html'
 
+function stripTags(input) {
+    return input.replace(/<[^>]*>?/gm, '')
+}
+
 export const sanitizeInput = (data) => {
     const sanitizedData = {}
     for (const [key, value] of Object.entries(data)) {
         if (typeof value === 'string') {
-            sanitizedData[key] = sanitizeHtml(value, { allowedTags: ['b'], allowedAttributes: [] })
+            if (key === 'text') {
+                sanitizedData[key] = sanitizeHtml(value, { allowedTags: ['b'], allowedAttributes: [] })
+            } else {
+                sanitizedData[key] = stripTags(value)
+            }
         } else {
             sanitizedData[key] = value
         }
