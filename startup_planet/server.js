7d9496263cd3b3c617f6f8a1e56682313c9d5c2b
faisalljavid <faisalljavid@gmail.com>
2025-07-30 00:41:53 +0530

    Add API endpoint for startups with filtering capabilities based on industry, country, continent, funding status, and MVP status. Also, introduce data file for startup information.

--- startup_planet/server.js
+++ startup_planet/server.js
@@ -1,8 +1,50 @@
-import express from "express"
+import express from 'express'
+import { startups } from './data/data.js'
 
 const PORT = 8000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server connected on PORT ${PORT}`)
-)
+app.get('/api', (req, res) => {
+
+    let filteredData = startups
+
+    const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
+
+    if (industry) {
+        filteredData = filteredData.filter(startup => {
+            return startup.industry.toLowerCase() === industry.toLowerCase()
+        })
+    }
+
+    if (country) {
+        filteredData = filteredData.filter(startup => {
+            return startup.country.toLowerCase() === country.toLowerCase()
+        })
+    }
+    if (continent) {
+        filteredData = filteredData.filter(startup => {
+            return startup.continent.toLowerCase() === continent.toLowerCase()
+        })
+    }
+
+    if (is_seeking_funding) {
+        filteredData = filteredData.filter(startup => {
+            return startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
+        })
+    }
+
+    if (has_mvp) {
+        filteredData = filteredData.filter(startup => {
+            return startup.has_mvp.toLowerCase() === JSON.parse(has_mvp.toLowerCase())
+        })
+    }
+
+
+    res.json(filteredData)
+
+})
+
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
+
+
